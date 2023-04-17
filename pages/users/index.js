7e49d664a477ad242f311f35628885c1215c14e9
Yeasir Arafat <yeasirar@gmail.com>
2023-04-18 00:31:29 +0600

    add users page

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,10 +1,306 @@
+import { Fragment } from "react";
+import { Menu, Popover, Transition } from "@headlessui/react";
+import Users from "../../components/users/users";
+import {
+  ChatBubbleLeftEllipsisIcon,
+  CodeBracketIcon,
+  EllipsisVerticalIcon,
+  EyeIcon,
+  FlagIcon,
+  HandThumbUpIcon,
+  MagnifyingGlassIcon,
+  PlusIcon,
+  ShareIcon,
+  StarIcon,
+} from "@heroicons/react/20/solid";
+import {
+  ArrowTrendingUpIcon,
+  Bars3Icon,
+  BellIcon,
+  FireIcon,
+  HomeIcon,
+  UserGroupIcon,
+  XMarkIcon,
+  InformationCircleIcon,
+  ChatBubbleBottomCenterTextIcon,
+} from "@heroicons/react/24/outline";
 
-const Users = () => {
-    return (
-        <div>
-            <h1>Users</h1>
-        </div>
-    )
+import { BsFillCaretDownFill as Upvote } from "react-icons/bs";
+import { BsFillCaretUpFill as Downvote } from "react-icons/bs";
+
+import { Button } from "@mui/material";
+import Image from "next/image";
+import { Input } from "@material-tailwind/react";
+const user = {
+  name: "Yeasir Arafat",
+  email: "yeasir402@gmail.com",
+  imageUrl: "/images/yeasir.jpg",
+};
+const navigation = [
+  { name: "Home", href: "#", icon: HomeIcon, current: true },
+  { name: "Questions", href: "#", icon: FireIcon, current: false },
+  { name: "Tags", href: "#", icon: ArrowTrendingUpIcon, current: false },
+  { name: "Users", href: "#", icon: UserGroupIcon, current: false },
+  {
+    name: "About",
+    href: "/about",
+    icon: InformationCircleIcon,
+    current: false,
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+    icon: ChatBubbleBottomCenterTextIcon,
+    current: false,
+  },
+];
+const userNavigation = [
+  { name: "Your Profile", href: "#" },
+  { name: "Settings", href: "#" },
+  { name: "Sign out", href: "#" },
+];
+const communities = [
+  { name: "React", href: "#" },
+  { name: "Flutter", href: "#" },
+  { name: "Next JS", href: "#" },
+  { name: "Android", href: "#" },
+  { name: "JavaScript", href: "#" },
+  { name: "Java", href: "#" },
+];
+
+const quicklinks = [
+  { name: "Install React", href: "#" },
+  { name: "Install Node", href: "#" },
+  { name: "Setup Project", href: "#" },
+];
+const tabs = [
+  { name: "Recent", href: "#", current: true },
+  { name: "Most Viewed", href: "#", current: false },
+  { name: "Most Answers", href: "#", current: false },
+];
+
+const questions = [
+  {
+    id: "81614",
+    likes: "29",
+    replies: "11",
+    views: "2.7k",
+    author: {
+      name: "Yeasir Arafat",
+      imageUrl: "/images/yeasir.jpg",
+      href: "#",
+    },
+    date: "December 9 at 11:43 AM",
+    datetime: "2020-12-09T11:43:00",
+    href: "#",
+    title: "How to cleanly make multiple elements movable anywhere?",
+    body: `
+      <p>The problem is: I don't wanna test for every single date field, like year, month, day, hour, minute, etc., but if I simply compare the two values, it'll always display both values, since the time precision goes beyond seconds, making the dates different even though I never edited that particular post.</p>
+    `,
+  },
+  {
+    id: "81614",
+    likes: "29",
+    replies: "11",
+    views: "2.7k",
+    author: {
+      name: "Yeasir Arafat",
+      imageUrl: "/images/yeasir.jpg",
+      href: "#",
+    },
+    date: "December 9 at 11:43 AM",
+    datetime: "2020-12-09T11:43:00",
+    href: "#",
+    title: "How to create 'Published' and 'Last edited' fields?",
+    body: `
+      <p>The problem is: I don't wanna test for every single date field, like year, month, day, hour, minute, etc., but if I simply compare the two values, it'll always display both values, since the time precision goes beyond seconds, making the dates different even though I never edited that particular post.</p>
+    `,
+  },
+  {
+    id: "81614",
+    likes: "29",
+    replies: "11",
+    views: "2.7k",
+    author: {
+      name: "Yeasir Arafat",
+      imageUrl: "/images/yeasir.jpg",
+      href: "#",
+    },
+    date: "December 9 at 11:43 AM",
+    datetime: "2020-12-09T11:43:00",
+    href: "#",
+    title:
+      "ChatBot - Trouble using custom gpt_index and langchain libraries for creating a GPT-3 based search index",
+    body: `
+      <p>The problem is: I don't wanna test for every single date field, like year, month, day, hour, minute, etc., but if I simply compare the two values, it'll always display both values, since the time precision goes beyond seconds, making the dates different even though I never edited that particular post.</p>
+    `,
+  },
+  // More questions...
+];
+const whoToFollow = [
+  {
+    name: "Yeasir Arafat",
+    handle: "yeasirar",
+    href: "#",
+    imageUrl: "/images/yeasir.jpg",
+  },
+  {
+    name: "Yeasir Arafat",
+    handle: "yeasirar",
+    href: "#",
+    imageUrl: "/images/yeasir.jpg",
+  },
+  {
+    name: "Yeasir Arafat",
+    handle: "yeasirar",
+    href: "#",
+    imageUrl: "/images/yeasir.jpg",
+  },
+  // More people...
+];
+const trendingPosts = [
+  {
+    id: 1,
+    user: {
+      name: "Yeasir Arafat",
+      imageUrl: "/images/yeasir.jpg",
+    },
+    body: "How to cleanly make multiple elements movable anywhere?",
+    comments: 291,
+  },
+  {
+    id: 1,
+    user: {
+      name: "Yeasir Arafat",
+      imageUrl: "/images/yeasir.jpg",
+    },
+    body: "How to cleanly make multiple elements movable anywhere?",
+    comments: 291,
+  },
+  {
+    id: 1,
+    user: {
+      name: "Yeasir Arafat",
+      imageUrl: "/images/yeasir.jpg",
+    },
+    body: "How to cleanly make multiple elements movable anywhere?",
+    comments: 291,
+  },
+  {
+    id: 1,
+    user: {
+      name: "Yeasir Arafat",
+      imageUrl: "/images/yeasir.jpg",
+    },
+    body: "How to cleanly make multiple elements movable anywhere?",
+    comments: 291,
+  },
+  {
+    id: 1,
+    user: {
+      name: "Yeasir Arafat",
+      imageUrl: "/images/yeasir.jpg",
+    },
+    body: "How to cleanly make multiple elements movable anywhere?",
+    comments: 291,
+  },
+
+  // More posts...
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
 }
 
-export default Users
+export default function AskQuestion() {
+  return (
+    <>
+      <div className="min-h-full">
+        <div className="py-10">
+          <div className="mx-auto max-w-3xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-12 lg:gap-8 lg:px-8">
+            <div className="hidden lg:col-span-3 lg:block xl:col-span-2">
+              <nav
+                aria-label="Sidebar"
+                className="sticky top-4 divide-y divide-gray-300"
+              >
+                <div className="space-y-1 pb-8">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        item.current
+                          ? "bg-gray-200 text-gray-900"
+                          : "text-gray-700 hover:bg-gray-50",
+                        "group flex items-center px-3 py-2 text-sm font-medium rounded-md"
+                      )}
+                      aria-current={item.current ? "page" : undefined}
+                    >
+                      <item.icon
+                        className={classNames(
+                          item.current
+                            ? "text-gray-500"
+                            : "text-gray-400 group-hover:text-gray-500",
+                          "flex-shrink-0 -ml-1 mr-3 h-6 w-6"
+                        )}
+                        aria-hidden="true"
+                      />
+                      <span className="truncate">{item.name}</span>
+                    </a>
+                  ))}
+                </div>
+                <div className="pt-10 pb-5">
+                  <p
+                    className="px-3 text-sm font-medium text-gray-500"
+                    id="communities-headline"
+                  >
+                    Popular Categories
+                  </p>
+                  <div
+                    className="mt-3 space-y-2"
+                    aria-labelledby="communities-headline"
+                  >
+                    {communities.map((community) => (
+                      <a
+                        key={community.name}
+                        href={community.href}
+                        className="group flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900"
+                      >
+                        <span className="truncate">{community.name}</span>
+                      </a>
+                    ))}
+                  </div>
+                </div>
+                <div className="pt-10">
+                  <p
+                    className="px-3 text-sm font-medium text-gray-500"
+                    id="communities-headline"
+                  >
+                    Quick Links
+                  </p>
+                  <div
+                    className="mt-3 space-y-2"
+                    aria-labelledby="communities-headline"
+                  >
+                    {quicklinks.map((ql) => (
+                      <a
+                        key={ql.name}
+                        href={ql.href}
+                        className="group flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900"
+                      >
+                        <span className="truncate">{ql.name}</span>
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </nav>
+            </div>
+            <main className="lg:col-span-12 xl:col-span-10">
+            <Users/>
+            </main>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
