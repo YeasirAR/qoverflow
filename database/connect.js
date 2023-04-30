446619540969725490c87d13d537442ba0a6c8c9
Yeasir Arafat <yeasirar@gmail.com>
2023-04-30 06:43:58 +0600

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = "mongodb+srv://yeasir:yeasir@cluster0.ujeivtm.mongodb.net/qoverflow?retryWrites=true&w=majority"
+const MONGODB_URI = "mongodb+srv://yeasir:yeasir@cluster0.ujeivtm.mongodb.net/qverflow?retryWrites=true&w=majority"
 
 async function connect() {
   await mongoose.connect(MONGODB_URI, {
