33f22b2312fcc2b46997df50eb9bfe4e3786154d
Yeasir Arafat <yeasirar@gmail.com>
2024-05-09 16:44:34 +0600

    Extended Encryption

--- custom_encryption_algorithm/custom_encryption_algorithm.js
+++ custom_encryption_algorithm/custom_encryption_algorithm.js
@@ -3,52 +3,63 @@ export class CustomEncryptionAlgorithm {
   static key1 =
     "*OTC#Y#1kTuU5=_O+ilm3-jzd7G_qb5a|2z1!#oP4?3y$jEYn!F8A0+uJe^iY*L_PN1?8W!dwXGbetk$z#Hvx-M|Ky?NV9xiVe6|u9JN9oI-sbt9wswS*-xPOKI$specJ$FfK$l1!YyISaoPAaNpe$WXc&WHt!V6$01h+Cidk=SgYzj2o&mw=OHnj#%X9c95q+TUp?p@t0^!MeEEkW&YEwM?IXfsZCVk&4dhMy=febmRlIcYp^+0q2=TYm";
   static key2 =
-    "6b@+YUQwOzxyIZUxmkjZnwv&UGqNI$tJd3FP7PQLsio+dM!^wI_v3Mnz5ZE@YH+g99gj3gFRS+z-#RZ?JQQX$?4$SL@nY!GZ!Tl=4Y@t?S3VAzhUogRTADDOd7Cl6Gi%";
-  static key3 = "6+4_2+7_3+3_1+5";
-
+    "4.JKj:>bIPL-fC7*bw9,:3$_;m)5V+4&_ID6X%vylh0{y\fVjDwuYHfgF#YZ1r0LN*9@[3(@j;$,f5;o!rPgdX{1zf~oAi_NLh1V6tDD!m!3r|bnh£rMJ4v+?aHI6?Mmue&Napn382=2t[N%P|$£6b@+YUQwOzxyIZUxmkjZnwv&UGqNI$tJd3FP7PQLsio+dM!^wI_v3Mnz5ZE@YH+g99gj3gFRS+z-#RZ?JQQX$?4$SL@nY!GZ!Tl=4Y";
+  static key3 = "9+4_2+7_3+3_1+8";
   encrypt(text) {
+    
     const startPadding = Number(CustomEncryptionAlgorithm.key3[0]);
-    const endPadding = Number(CustomEncryptionAlgorithm.key3[CustomEncryptionAlgorithm.key3.length - 1]);
-    const key3_interval = [Number(CustomEncryptionAlgorithm.key3[2]),Number(CustomEncryptionAlgorithm.key3[6]),
-                            Number(CustomEncryptionAlgorithm.key3[10]),
-                          ]
-    const key3_padding = [Number(CustomEncryptionAlgorithm.key3[4]),Number(CustomEncryptionAlgorithm.key3[8]),
-                            Number(CustomEncryptionAlgorithm.key3[12]),
-                          ]
-    const key3_len = 3
+    const endPadding = Number(
+      CustomEncryptionAlgorithm.key3[CustomEncryptionAlgorithm.key3.length - 1]
+    );
+    const key3_interval = [
+      Number(CustomEncryptionAlgorithm.key3[2]),
+      Number(CustomEncryptionAlgorithm.key3[6]),
+      Number(CustomEncryptionAlgorithm.key3[10]),
+    ];
+    const key3_padding = [
+      Number(CustomEncryptionAlgorithm.key3[4]),
+      Number(CustomEncryptionAlgorithm.key3[8]),
+      Number(CustomEncryptionAlgorithm.key3[12]),
+    ];
+    const key3_len = 3;
     let paddedText = "";
     for (let i = 0; i < startPadding; i++) {
-      paddedText += String.fromCharCode(Crypto.randomBytes(1)[0] % 128);
+      paddedText += String.fromCharCode(Crypto.randomBytes(1)[0] % 256);
     }
-    let curr_i = 0
-    let curr_pad = 0
-    for(let i=0;i<text.length;i++){
-      if(curr_i== key3_interval[curr_pad%key3_len]){
-        for(let j=0;j<key3_padding[curr_pad%key3_len];j++){
-          paddedText += String.fromCharCode(Crypto.randomBytes(1)[0] % 128);
+    let curr_i = 0;
+    let curr_pad = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (curr_i == key3_interval[curr_pad % key3_len]) {
+        for (let j = 0; j < key3_padding[curr_pad % key3_len]; j++) {
+          paddedText += String.fromCharCode(Crypto.randomBytes(1)[0] % 256);
         }
-        curr_pad += 1
-        curr_i = 0
+        curr_pad += 1;
+        curr_i = 0;
       }
       paddedText += text[i];
-      curr_i += 1
+      curr_i += 1;
     }
     for (let i = 0; i < endPadding; i++) {
-      paddedText += String.fromCharCode(Crypto.randomBytes(1)[0] % 128);
+      paddedText += String.fromCharCode(Crypto.randomBytes(1)[0] % 256);
     }
-    
+
     let cipherText = "";
-    
+
     for (let i = 0; i < paddedText.length; i++) {
       const charCode = paddedText.charCodeAt(i);
       const first_shift = CustomEncryptionAlgorithm.key1.charCodeAt(
         i % CustomEncryptionAlgorithm.key1.length
       );
-      const second_shift = CustomEncryptionAlgorithm.key2.charCodeAt(first_shift);
-      const newCode = (charCode + second_shift) % 128;
+      const second_shift =
+        CustomEncryptionAlgorithm.key2.charCodeAt(first_shift);
+      const newCode =
+        (charCode +
+          second_shift +
+          first_shift) %
+        256;
       cipherText += String.fromCharCode(newCode);
     }
-    return [paddedText,cipherText];
+    return [paddedText, cipherText];
   }
 
   decrypt(text) {
@@ -59,32 +70,43 @@ export class CustomEncryptionAlgorithm {
       const first_shift = CustomEncryptionAlgorithm.key1.charCodeAt(
         i % CustomEncryptionAlgorithm.key1.length
       );
-      const second_shift = CustomEncryptionAlgorithm.key2.charCodeAt(first_shift);
-      const newCode = (charCode - second_shift + 128) % 128;
+      const second_shift =
+        CustomEncryptionAlgorithm.key2.charCodeAt(first_shift);
+      const newCode =
+        (charCode -
+          second_shift -
+          first_shift + 512) %
+        256;
       plainText += String.fromCharCode(newCode);
     }
     const startPadding = Number(CustomEncryptionAlgorithm.key3[0]);
-    const endPadding = Number(CustomEncryptionAlgorithm.key3[CustomEncryptionAlgorithm.key3.length - 1]);
-    const key3_interval = [Number(CustomEncryptionAlgorithm.key3[2]),Number(CustomEncryptionAlgorithm.key3[6]),
-                            Number(CustomEncryptionAlgorithm.key3[10]),
-                          ]
-    const key3_padding = [Number(CustomEncryptionAlgorithm.key3[4]),Number(CustomEncryptionAlgorithm.key3[8]),
-                            Number(CustomEncryptionAlgorithm.key3[12]),
-                          ]
-    const key3_len = 3
+    const endPadding = Number(
+      CustomEncryptionAlgorithm.key3[CustomEncryptionAlgorithm.key3.length - 1]
+    );
+    const key3_interval = [
+      Number(CustomEncryptionAlgorithm.key3[2]),
+      Number(CustomEncryptionAlgorithm.key3[6]),
+      Number(CustomEncryptionAlgorithm.key3[10]),
+    ];
+    const key3_padding = [
+      Number(CustomEncryptionAlgorithm.key3[4]),
+      Number(CustomEncryptionAlgorithm.key3[8]),
+      Number(CustomEncryptionAlgorithm.key3[12]),
+    ];
+    const key3_len = 3;
 
     let unpaddedText = "";
-    let curr_i = 0
-    let curr_pad = 0
-    for(let i=startPadding;i<plainText.length-endPadding;i++){
-      if(curr_i== key3_interval[curr_pad%key3_len]){
-        i += key3_padding[curr_pad%key3_len]
-        curr_pad += 1
-        curr_i = 0
+    let curr_i = 0;
+    let curr_pad = 0;
+    for (let i = startPadding; i < plainText.length - endPadding; i++) {
+      if (curr_i == key3_interval[curr_pad % key3_len]) {
+        i += key3_padding[curr_pad % key3_len];
+        curr_pad += 1;
+        curr_i = 0;
       }
       unpaddedText += plainText[i];
-      curr_i += 1
+      curr_i += 1;
     }
-    return [unpaddedText,plainText];
+    return [unpaddedText, plainText];
   }
 }
--- pages/test_encryption/index.js
+++ pages/test_encryption/index.js
@@ -1,39 +1,49 @@
-import { use, useState } from "react"
-import { useEffect } from "react"
-import {CustomEncryptionAlgorithm} from "@/custom_encryption_algorithm/custom_encryption_algorithm"
+import { use, useState } from "react";
+import { useEffect } from "react";
+import { CustomEncryptionAlgorithm } from "@/custom_encryption_algorithm/custom_encryption_algorithm";
 import { set } from "mongoose";
+Crypto = require("crypto");
 
 const Func = () => {
-    const [paddedText, setPaddedText] = useState("");
-    const [encryptedText, setEncryptedText] = useState("");
-    const [decryptedText, setDecryptedText] = useState("");
-    const [originalText, setOriginalText] = useState("");
-    const [unpaddedText, setUnpaddedText] = useState("");
-    useEffect(() => {
-        const ENCR = new CustomEncryptionAlgorithm();
-        const encrypt = ENCR.encrypt;
-        const decrypt = ENCR.decrypt;
-        const text = "This is a Secret message! It will be encrypted and decrypted using a custom algorithm."        
-        const [paddedText,encryptedText] = encrypt(text);
-        const [unpaddedText,decryptedText] = decrypt(encryptedText);
-        setEncryptedText(encryptedText);
-        setDecryptedText(decryptedText);
-        setPaddedText(paddedText);
-        setOriginalText(text);
-        setUnpaddedText(unpaddedText);
-        console.log(encryptedText); 
-        console.log(decryptedText); 
+  const [paddedText, setPaddedText] = useState("");
+  const [encryptedText, setEncryptedText] = useState("");
+  const [decryptedText, setDecryptedText] = useState("");
+  const [originalText, setOriginalText] = useState("");
+  const [unpaddedText, setUnpaddedText] = useState("");
+  useEffect(() => {
+    let key1 = "";
+    let key2 = "";
+    for (let i = 0; i < 256; i++) {
+      key1 += String.fromCharCode(Crypto.randomBytes(1)[0] % 256);
+      key2 += String.fromCharCode(Crypto.randomBytes(1)[0] % 256);
+    }
+    console.log(key1);
+    console.log(key2);
+    const ENCR = new CustomEncryptionAlgorithm();
+    const encrypt = ENCR.encrypt;
+    const decrypt = ENCR.decrypt;
+    const text =
+      "This is a Secret message! It will be encrypted and decrypted using a custom algorithm.";
+    const [paddedText, encryptedText] = encrypt(text);
+    const [unpaddedText, decryptedText] = decrypt(encryptedText);
+    setEncryptedText(encryptedText);
+    setDecryptedText(decryptedText);
+    setPaddedText(paddedText);
+    setOriginalText(text);
+    setUnpaddedText(unpaddedText);
+    console.log(encryptedText);
+    console.log(decryptedText);
+  }, []);
 
-    }, [])
-    
-  return <div>
-        <h1 className="text-center">Original Text: {originalText}</h1>
-        <h1 className="text-center">Padded Text: {paddedText}</h1>
-        <h1 className="text-center">Encrypted Text: {encryptedText}</h1>
-        <h1 className="text-center">Decrypted Text: {decryptedText}</h1>
-        <h1 className="text-center">Unpadded Text: {unpaddedText}</h1>
+  return (
+    <div>
+      <h1 className="text-center">Original Text: {originalText}</h1>
+      <h1 className="text-center">Padded Text: {paddedText}</h1>
+      <h1 className="text-center">Encrypted Text: {encryptedText}</h1>
+      <h1 className="text-center">Decrypted Text: {decryptedText}</h1>
+      <h1 className="text-center">Unpadded Text: {unpaddedText}</h1>
+    </div>
+  );
+};
 
-  </div>
-}
-
-export default Func
+export default Func;
